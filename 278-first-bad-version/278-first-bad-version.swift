d5541fd3deb0c3e612ff4a79be4df7071723820f
wonhee <wonhee010@gmail.com>
2022-02-08 19:41:01 +0900

    Time: 2 ms (52.42%), Space: 13.8 MB (48.60%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.swift
+++ 278-first-bad-version/278-first-bad-version.swift
@@ -5,14 +5,17 @@
 
 class Solution : VersionControl {
     func firstBadVersion(_ n: Int) -> Int {
-        var currentVersion = n - 1
-        while(currentVersion >= 1) {
-            if isBadVersion(currentVersion) {
-                currentVersion -= 1
+        var start = 0
+        var end = n - 1
+        var mid = (start + end) / 2
+        while(start <= end) {
+            if isBadVersion(mid) {
+                end = mid - 1
             } else {
-                return currentVersion + 1
+                start = mid + 1
             }
+            mid = (start + end) / 2
         }
-        return currentVersion + 1
+        return mid + 1
     }
 }
